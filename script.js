27a60800b65a7bf2b8ddd93115ac762895413bd7
Jasa CIT <99124429+citnias@users.noreply.github.com>
2026-01-17 05:01:09 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -17,20 +17,30 @@ document.getElementById('upload').addEventListener('click', () => {
   let dataURL = canvas.toDataURL('image/png');
   let blob = dataURItoBlob(dataURL);
 
+  // Ambil catatan dari input
+  let note = document.getElementById('note').value.trim();
+  if (note === "") note = "tanpa_catatan";
+
+  // Nama file dengan timestamp + catatan
+  let fileName = "foto_bukti_" + Date.now() + "_" + note + ".png";
+
   gapi.load("client:auth2", function() {
     gapi.auth2.init({
-      client_id: "YOUR_CLIENT_ID.apps.googleusercontent.com" // ganti dengan Client ID Anda
+      client_id: "928691431108-6bkm4ml6d0jric5nk2ppkf98e091v7kp.apps.googleusercontent.com" // ganti dengan Client ID Anda
     });
     gapi.client.load("drive", "v3", function() {
       gapi.auth2.getAuthInstance().signIn().then(() => {
-        var fileMetadata = { name: "foto_bukti_" + Date.now() + ".png" };
+        var fileMetadata = { 
+          name: fileName,
+          parents: ["https://drive.google.com/drive/folders/1mdg3uA8NFs8c8yx-KqGLaVD245B28Tu0"] // ganti dengan ID folder Drive tujuan
+        };
         var media = { mimeType: "image/png", body: blob };
         gapi.client.drive.files.create({
           resource: fileMetadata,
           media: media,
           fields: "id"
         }).then(response => {
-          alert("Foto berhasil diupload ke Google Drive! ID: " + response.result.id);
+          alert("Foto berhasil diupload ke folder Drive! ID: " + response.result.id);
         });
       });
     });
